79e5573a68af37f819d2e60f35746c25184ffff0
ggpereira <ggomesper@gmail.com>
2019-08-26 23:35:26 -0300

    teste

--- src/services/root/routes.ts
+++ src/services/root/routes.ts
@@ -5,7 +5,7 @@ export default [
         path: "/",
         method: "get",
         handler: async (req: Request, res: Response) => {
-            res.send("Hello World!");
+            res.send("Bem vindo! Tá funcionando");
         }
     }
 ];
