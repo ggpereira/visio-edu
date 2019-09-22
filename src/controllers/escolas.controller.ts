b50491e1cd93a1c85f12df226e42e39099e17239
ggpereira <ggomesper@gmail.com>
2019-09-21 23:15:49 -0300

    consultas e filtros para endpoint escolas

--- src/controllers/escolas.controller.ts
+++ src/controllers/escolas.controller.ts
@@ -1,43 +1,102 @@
 import { Request, Response } from 'express';
-import { getConnection, Connection } from 'typeorm';
+import { getConnection, Connection, SelectQueryBuilder } from 'typeorm';
 import { IEscola } from '../models/escolas';
-import { isNull } from 'util';
-import { objectExpression, isNullLiteral } from '@babel/types';
+import { create } from 'domain';
+
  
 // Processa a busca por escolas baseado em filtros
-export async function getEscolas(req: Request, res: Response): Promise<Response> {
-    const conn: Connection = getConnection()
-    // URL params
+// A busca pode ser realizada pelo nome, cidade e estado, ambos parâmetros são opcionais
+// A paginação só é feita se não há nenhum filtro especificado para consulta
+export async function getEscolas(req: Request, res: Response): Promise<Response>{
+    const conn: Connection = getConnection();
     const page = parseInt(req.query.page) || 1;
     const per_page = parseInt(req.query.per_page) || 70;
-    const filterBy = req.query.filterBy;
-    const filter = req.query.filter;
-    const orderBy = req.query.orderBy;
+    const filterByNome = req.query.no_entidade;
+    const filterByEstado = req.query.estado;
+    const filterByMunicipio = req.query.municipio;
+    const orderBy = req.query.orderBy || 'nome';
     const order = req.query.order || 'ASC';
 
-    // Busca o total de registros na tela
-    const results = await conn.query('SELECT COUNT(co_entidade) as total FROM escolas')
-    const maxPages = Math.ceil(results[0].total / per_page)
+    // Constrói a consulta base
+    let queryBuilder = conn.createQueryBuilder();
+    queryBuilder.select("escolas.*, estados.estado as 'cidade', cidades.municipio as 'municipio'").from("escolas", "escolas")
+        .innerJoin("estados", "estados", "estados.codigo = escolas.co_uf")
+        .innerJoin("cidades", "cidades", "cidades.codigo = escolas.co_municipio");
 
-    // Calcula o offset das linhas
-    const offset = calculaOffset(page, per_page);
-    var builder = conn.createQueryBuilder()
-    builder.select('*').from('escolas', 'escolas').limit(per_page).offset(offset)
-    
-    if(filterBy && filter){
-        builder = queryWhere(filterBy, filter, builder);
+    // Adiciona os filtros e parâmetros especificados na query
+    queryBuilder = setQueryFilters(queryBuilder, {page, per_page, filterByNome, filterByEstado, filterByMunicipio, orderBy, order});
+
+    let response: any = {};
+
+    // Se nenhum filtro foi especificado a query é paginada
+    if(queryBuilder.getQueryAndParameters()[1].length <= 0) {
+        let paginationData = await paginate(page, per_page);
+        queryBuilder.limit(paginationData.per_page).offset(paginationData.offset);
+        response.perPage = per_page;
+        response.page = page;
+        response.maxPages = paginationData.maxPages;
     }
 
-    const escolas: IEscola[] = await builder.execute()
-    const response = {
-        data: escolas,
-        per_page: per_page,
-        maxPages: maxPages,
-        filter: filter,
-        filterBy: filterBy,
-    }
+    // Obtém os dados da consulta
+    const dataEscolas = await queryBuilder.execute().catch((e) => {
+        return res.status(500).send('ocorreu um erro inesperado');
+    });
+
+    response.data = dataEscolas;
 
     return res.json(response);
+}
+
+// Calcula total de paginas e offset 
+// Retorna objeto com quantidade de registros por página, total de páginas e offset
+async function paginate(page: number, per_page: number): Promise <any> {
+    const conn = getConnection();
+    let rs = await conn.query('SELECT COUNT(co_entidade) as total FROM escolas');
+    const maxPages = Math.ceil(rs[0].total/per_page);
+    const offset = calculaOffset(page, per_page);
+    return {
+        per_page: per_page,
+        maxPages: maxPages,
+        offset: offset,
+    }
+}
+
+// Adiciona os filtros especificados na query
+function setQueryFilters(queryBuilder: SelectQueryBuilder<any>, queryParams: any): SelectQueryBuilder<any> {
+    // filtro por nome
+    if(queryParams.filterByNome) {
+        queryBuilder = createQueryFilter(queryBuilder, "escolas.no_entidade LIKE :nome", {nome: `%${queryParams.filterByNome}%`});
+    } 
+
+    // filtro por estado
+    if(queryParams.filterByEstado) {
+        queryBuilder = createQueryFilter(queryBuilder, "estados.estado = :estado", {estado: queryParams.filterByEstado});
+    }
+
+    // filtro por municipio
+    if(queryParams.filterByMunicipio){
+        queryBuilder = createQueryFilter(queryBuilder, "cidades.municipio = :municipio", {municipio: queryParams.filterByMunicipio});
+    }
+
+    // ordenação dos registros
+    switch(queryParams.orderBy){
+        case 'municipio':
+            queryBuilder.orderBy('cidades.municipio', queryParams.order);
+            break;
+        case 'estado':
+            queryBuilder.orderBy('estados.estado', queryParams.order);
+            break; 
+        case 'nome':
+            queryBuilder.orderBy('escolas.no_entidade', queryParams.order);
+    }
+
+    return queryBuilder
+}
+
+// Adiciona o where na query
+// Se já houver algum parâmetro na query usa query.andWhere()
+function createQueryFilter(queryBuilder: SelectQueryBuilder<any>, queryWhere: string, filter: any){
+    return queryBuilder.getQueryAndParameters()[1].length <= 0 ? queryBuilder.where(queryWhere, filter) : queryBuilder.andWhere(queryWhere, filter);
 }
 
 
@@ -45,16 +104,7 @@ function calculaOffset(page: number, limit: number): number{
     return ((page - 1) * limit) + 1;
 }
 
-function queryWhere(filterBy: String, filter: String, builder: any): any{
-    if(filterBy === "cidade"){
-        return builder.leftJoin("cidades", "cidades", "cidades.codigo = escolas.co_municipio").where("cidades.municipio = :nome",{nome: filter});
-    }
-    if(filterBy === "estado"){
-        return builder.leftJoin("estados", "estados", "estados.codigo = escolas.co_uf").where("estados.estado = :nome",{nome: filter});
-    }
-    return builder;
-}
-
+// Recupera registros de escolas pelo código da entidade
 export async function  getEscolaByID(req: Request, res: Response): Promise<Response>{
     const conn = getConnection()
     const queryBuilder =  conn.createQueryBuilder();
